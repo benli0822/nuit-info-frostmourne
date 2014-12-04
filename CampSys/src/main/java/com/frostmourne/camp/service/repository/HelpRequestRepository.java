b2aa741ad2f3ea9e38be18bd7b6cb4bef9006b44
jamescheng16 <jamescheng16@gmail.com>
2014-12-05 00:24:03 +0100

    repository

---
+++ CampSys/src/main/java/com/frostmourne/camp/service/repository/HelpRequestRepository.java
@@ -0,0 +1,11 @@
+package com.frostmourne.camp.service.repository;
+
+import com.frostmourne.camp.domain.HelpRequest;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jamesRMBP on 05/12/14.
+ */
+public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {
+
+}
---
+++ CampSys/src/main/java/com/frostmourne/camp/service/repository/InformationRepository.java
@@ -0,0 +1,10 @@
+package com.frostmourne.camp.service.repository;
+
+import com.frostmourne.camp.domain.Information;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jamesRMBP on 05/12/14.
+ */
+public interface InformationRepository extends CrudRepository<Information, Long> {
+}
